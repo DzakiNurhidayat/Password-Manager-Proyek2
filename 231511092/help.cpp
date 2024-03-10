3153562288fd2d493b142b0c1c352b680f947b96
Sifa Wafiqna <sifa.wafiqna@gmail.com>
2024-03-10 21:20:00 +0700

    revisi help

--- 231511092/help.cpp
+++ 231511092/help.cpp
@@ -2,41 +2,48 @@
 
 using namespace std;
 
-int main()
-{
-	int help, no;
-	
-	cout<<"Help/More info?"<<endl<<"Ketik: 1 for Yes/ 2 for No"<<endl;
-	cin>>help;
-	
-	if (help==1)
-	{
-		cout<<"1. Bagaimana cara melihat list password?"<<endl<<"2. Ingin merubah password?"<<endl<<"3. Ingin menghapus password?"<<endl;
-		cin>>no;
-		
-		cout<<endl;
-		
-		if (no==1)
-		{
-			cout<<endl<<"ketik menu saat awal masuk aplikasi dan input nomor 3 saat memilih password library"<<endl;
-		}
-		else
-		{
-			if (no==2)
-			{
-				cout<<"pilih menu untuk merubah password";
-			}
-			else
-			{
-				cout<<"Pilih menu untuk menghapus password";
-			}
-		}
-	}
-	else
-	{
-		if (help==2)
-		{
-			return 0;
-		}
-	}
+int main() {
+  int help, no;
+
+  // Menampilkan menu bantuan
+  cout << "Help/More info?" << endl;
+  cout << "Ketik: 1 untuk Ya / 2 untuk Tidak" << endl;
+  cout <<endl;
+  cin >> help;
+  cout << endl;
+
+  // Memproses pilihan bantuan
+  if (help == 1) {
+    // Menampilkan daftar pilihan
+    cout << "1. Bagaimana cara melihat list password?" << endl;
+    cout << "2. Ingin merubah password?" << endl;
+    cout << "3. Ingin menghapus password?" << endl;
+    cout<<endl;
+    cin >> no;
+
+    cout << endl;
+
+    // Menampilkan informasi berdasarkan pilihan
+    switch (no) {
+      case 1:
+        cout << "Ketik menu saat awal masuk aplikasi dan input nomor 3 saat memilih password library" << endl;
+        break;
+      case 2:
+        cout << "Pilih menu untuk merubah password" << endl;
+        break;
+      case 3:
+        cout << "Pilih menu untuk menghapus password" << endl;
+        break;
+      default:
+        cout << "Pilihan tidak valid" << endl;
+    }
+  } else if (help == 2) {
+    // Keluar dari program
+    return 0;
+  } else {
+    // Menampilkan pesan error
+    cout << "Pilihan tidak valid" << endl;
+  }
+
+  return 0;
 }
