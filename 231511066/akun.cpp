186bbd6d4d5d1ab6b6e7df06e7d9cd2388e37cd4
Afrizaaa017 <afrizaharapan@gmail.com>
2024-03-12 13:12:49 +0700

    :memo: Membuat Perulangan Login Register, Membuat Fitur Unique Username Pada Bagian Register, Membuat Fitur Add FIle Otomatis Setelah Register, Membuat Fitur Login 3x Kesempatan. :memo

--- 231511066/akun.cpp
+++ 231511066/akun.cpp
@@ -1,90 +1,182 @@
-
-
 #include <iostream>
-#include <string.h>
-// #include <iostream>
-// #include <fstream>
-// #include <string>
-
+#include <fstream>
+#include <string>
+#include <cstdlib> // Untuk exit()
+#include <cstring>
 
 using namespace std;
 
+typedef struct
+{
+    char username[30];
+    char password[30];
+    char pertanyaan[30];
+    char jawaban[30];
+} User;
 
-typedef struct {
-        char username[30];
-        char password[30];
-    } User;
-        
-    void registerUser() {
-        User user;
-        FILE *fp = fopen("users.dat", "ab");
+void Register()
+{
+    User user;
+    int i;
+    i = 0;
+    do
+    {
+        ofstream fp("user.txt", ios::out | ios::app | ios::binary);
+        if (!fp)
+        {
+            perror("Error opening file");
+            exit(1);
+        }
 
-        if (fp == NULL) {
+        cout << "Masukan Username: ";
+        cin >> user.username;
+
+        ifstream inputFile("user.txt", ios::binary);
+        if (!inputFile)
+        {
+            cerr << "Failed to open file for reading.\n";
+            return;
+        }
+
+
+
+        User existingUser;
+        bool isLoginSuccess = false;
+        while (inputFile.read(reinterpret_cast<char *>(&existingUser), sizeof(User)))
+        {
+            if (strcmp(existingUser.username, user.username) == 0)
+            {
+                inputFile.close();
+                isLoginSuccess = true;
+                break;
+                ;
+            }
+        }
+
+        if (isLoginSuccess == false)
+        {
+            inputFile.close(); 
+            cout << "Masukan Password: ";
+            cin >> user.password;
+            cout << "Buat Pertanyaan Untuk Lupa Password: ";
+            cin >> user.pertanyaan;
+            cout << "Jawaban Untuk Lupa Password: ";
+            cin >> user.jawaban;
+
+            ofstream outputFile("user.txt", ios::binary | ios::app);
+            if (!outputFile)
+            {
+                cerr << "Failed to open file for writing.\n";
+                return;
+            }
+
+            fp.write(reinterpret_cast<const char *>(&user), sizeof(User));
+            fp.close();
+            cout << "Registration successful!\n";
+
+            // MEMBUAT FILE USER
+            string fileName = string(user.username) + ".txt";
+            ofstream outFile(fileName.c_str(), ios::out | ios::app | ios::binary);
+
+            if (!outFile)
+            {
+                cout << "User belum menambahkan password\n";
+            }
+            else
+            {
+                outFile << "Username: " << user.username << "\n Password: " << user.password << "\n Pertanyaan: " << user.pertanyaan << "\nPassword: " << user.jawaban << endl;
+                outFile.close();
+            }
+            break;
+        }
+        else
+        {
+            cout << "Username Telah Digunakan, Harap Gunakan Username Lain \n"; 
+            cout << "Masa Percobaan " << (i + 1) << "x\n";
+        }
+        i++;
+    } while (i < 3);
+}
+
+void Login()
+{
+    User user, inputUser;
+    int i;
+    i = 0;
+    do
+    {
+        ifstream fp("user.txt", ios::binary);
+
+        if (!fp)
+        {
             perror("Error opening file");
             exit(1);
         }
 
         cout << "masukan username: ";
-        cin >> user.username;
-        cout << "masukan password: ";
-        cin >> user.password;
-
-
-        fwrite(&user, sizeof(User), 1, fp); 
-
-        fclose(fp);
-        printf("Registration successful!\n");
-    }
-
-void loginUser() {
-        User user, inputUser;
-        FILE *fp = fopen("users.dat", "rb"); 
-
-        if (fp == NULL) {
-            perror("Error opening file");
-            exit(1);
-        }
-
-          cout << "masukan username: ";
         cin >> inputUser.username;
         cout << "masukan password: ";
         cin >> inputUser.password;
 
-
-        while(fread(&user, sizeof(User), 1, fp)) {
-            if(strcmp(user.username, inputUser.username) == 0 && strcmp(user.password, inputUser.password) == 0) {
-                printf("Login successful!\n");
-                fclose(fp);
+        bool isLoginSuccess = false;
+        while (fp.read(reinterpret_cast<char *>(&user), sizeof(User)))
+        {
+            if (strcmp(user.username, inputUser.username) == 0 && strcmp(user.password, inputUser.password) == 0)
+            {
+                cout << "Login successful!\n";
+                isLoginSuccess = true;
                 return;
             }
         }
 
-        fclose(fp);
-        printf("Invalid username or password!\n");
-    }
+        if (isLoginSuccess == false)
+        {
+            cout << "===========================\n";
+            cout << "Username / Password Salah! (" << (i + 1) << "x)\n";
+            cout << "=============================\n";
+        }
 
+        i = i + 1;   
+    } while (i < 3); 
+
+    if (i == 3)
+    {
+
+        cout << "Login gagal setelah 3 kali percobaan. Silakan coba lagi nanti.\n";
+    }
+}
+
+void lupaPassword()
+{
+}
 
 int main()
 {
-      int option;
+    int choice;
 
-        printf("1. Register\n");
-        printf("2. Login\n");
-        printf("Enter your choic1e: ");
-        scanf("%d", &option);
+    do
+    {
+        cout << "1. Register\n";
+        cout << "2. Login\n";
+        cout << "3. Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
 
-        switch(option) {
-            case 1:
-                registerUser();
-                break;
-            case 2:
-                loginUser();
-                break;
-            default:
-                printf("Invalid option!\n");
+        switch (choice)
+        {
+        case 1:
+            Register();
+            break;
+        case 2:
+            Login();
+            break;
+        case 3:
+            cout << "Exiting..." << endl;
+            break;
+        default:
+            cout << "Invalid option! Please try again.\n";
         }
+    } while (choice != 3);
 
-        return 0;
     return 0;
-} 
-
+}
