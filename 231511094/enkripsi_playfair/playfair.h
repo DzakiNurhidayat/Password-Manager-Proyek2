e34a3882e237c57ac6b8302a126c03e9b0f085e0
Sulthan Aulia Rahman <sulthan.aulia.tif23@polban.ac.id>
2024-03-17 15:04:34 +0700

    :fire: remove unneeded function

--- 231511094/enkripsi_playfair/playfair.h
+++ 231511094/enkripsi_playfair/playfair.h
@@ -4,7 +4,6 @@
 using namespace std;
 
 void buatplayfairtable(string key, char playfairtable[10][10]);
-string format_text(const string &text);
 void cekposisi(char playfairtable[10][10], char c, int &row, int &col);
 string enkripsi_playfair(string plaintext, char playfairtabel[10][10]);
 string dekripsi_playfair(string ciphertext, char playfairtabel[10][10]);
