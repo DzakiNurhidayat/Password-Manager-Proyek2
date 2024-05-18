955c47f40054c46d53620f8b316e8b4a0548515f
Sulthan Aulia Rahman <sulthan.aulia.tif23@polban.ac.id>
2024-05-18 08:15:21 +0700

    :sparkles:Added decryption

--- 231511094/enkripsi_playfair/Decryption_Playfair.cpp
+++ 231511094/enkripsi_playfair/Decryption_Playfair.cpp
@@ -1,38 +1,114 @@
-// #include "playfair.h"
+#include "playfair.h"
 
-// // nanti fungsi ini ganti nama jadi main_decrypt
-// string decryption(string loggedInUser, string cipherText)
-// {
-//     char playfair_table[10][10];
-//     char lastchar;
-//     buatplayfairtable(loggedInUser, playfair_table);
-//     string decrypted_text;
+string decryption(string loggedInUser, string cipherText)
+{
 
-//     if (cipherText.length() % 2 != 0)//jika ganjil char akhir dihapus lalu dimasukan kembali setelah di dekripsi
-//     {
-//         lastchar = cipherText[cipherText.length() - 1];
-//         cipherText.pop_back();
-//         decrypted_text = dekripsi_playfair(cipherText, playfair_table);
-//         decrypted_text.push_back(lastchar);
-//     }
-//     else
-//     {
-//         decrypted_text = dekripsi_playfair(cipherText, playfair_table);
-//     }
+    string decrypted_text;
+    string hasil;
+    address head;
+    int size = 10;
+    hasil = unik(loggedInUser);
+    head = createTable(size, hasil);
 
-//     return decrypted_text;
-// }
+    char lastchar;
 
-// // aturan ketika enkripsi/dekripsi playfair
-// // 1. jika di kolom yang sama maka
-// // 2. jika di baris yang sama 
-// // 3. jika di tidak ada yang sama
-// // 4. jika kolom dan baris nya sama
+    if (cipherText.length() % 2 != 0)
+    {
+        lastchar = cipherText[cipherText.length() - 1];
+        cipherText.pop_back();
+        decrypted_text = decryption_playfair(head, cipherText);
+        decrypted_text.push_back(lastchar);
+    }
+    else
+    {
+        decrypted_text = decryption_playfair(head, cipherText);
+    }
 
-// string dekripsi_playfair(string ciphertext, char playfairtable[10][10])
-// {
-//     string decrypted_text;
+    return decrypted_text;
+}
 
-    
-//     return decrypted_text;
-// }
+string decryption_playfair(address Head, string ciphertext)
+{
+    string decrypted_text;
+    address address1, address2;
+
+    bool col, row;
+    char c1, c2;
+
+    for (int i = 0; i < ciphertext.length(); i += 2)
+    {
+        c1 = ciphertext[i];
+        c2 = ciphertext[i + 1];
+
+        address1 = searchingNode(Head, c1); // dapet address c1
+        address2 = searchingNode(Head, c2);
+
+        if (c1 != c2)
+        {
+            col = cek_vertikal(address1, address2);
+            row = cek_horizontal(address1, address2);
+            cout << " col = " << col << endl;
+            cout << " row = " << row << endl;
+            if (row == true) // same row
+            {
+
+                decrypted_text += samerow_dec(address1);
+                decrypted_text += samerow_dec(address2);
+            }
+            else if (col == true) // same col
+            {
+                decrypted_text += samecol_dec(address1);
+                decrypted_text += samecol_dec(address2);
+            }
+            else // no same
+            {
+                decrypted_text += kotak(address1, address2);
+                decrypted_text += kotak(address2, address1);
+            }
+        }
+        else
+        {
+            decrypted_text += c1;
+            decrypted_text += c2;
+        }
+    }
+    return decrypted_text;
+}
+
+char samerow_dec(address c1)
+{
+    char text;
+    address temp = c1;
+    if (temp->left == NULL)
+    {
+        while (temp->right != NULL)
+        {
+            c1 = temp->right;
+        }
+        text = temp->text;
+    }
+    else
+    {
+        text = temp->left->text;
+    }
+    return text;
+}
+
+char samecol_dec(address c1)
+{
+    char text;
+    address temp = c1;
+    if (temp->top == NULL)
+    {
+        while (temp->down != NULL)
+        {
+            temp = temp->down;
+        }
+        text = temp->text;
+    }
+    else
+    {
+        text = temp->top->text;
+    }
+    return text;
+}
